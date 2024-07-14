516af621d483038561a95b95fed60387904cd192
TakoyakiDayo <Takoyaki1051@outlook.jp>
2024-07-14 20:06:55 +0900

    good

--- earthsite/src/app/page.tsx
+++ earthsite/src/app/page.tsx
@@ -1,12 +1,7 @@
-import Link from 'next/link';
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <main>
-            <button><Link href="/about">about</Link></button><br />
-            <button><Link href="/status">status</Link></button><br />
-            <button><Link href="/main">main</Link></button>
+
     </main>
   );
 }
---
+++ zabix.md
@@ -0,0 +1,3 @@
+        youtube{""}
+        p2pAPI-v1-earthquake{""}
+        jma-wether
