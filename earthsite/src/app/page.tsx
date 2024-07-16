9030c8eab4968e971540b382a1a9441637c0873b
TakoyakiDayo <Takoyaki1051@outlook.jp>
2024-07-16 21:32:42 +0900

    good

--- earthsite/src/app/page.tsx
+++ earthsite/src/app/page.tsx
@@ -1,11 +1,25 @@
 import { Footer } from "@/components/Footer";
-
+import Link from "next/link";
+import classes from "@/components/Home.module.css";
 export default function Home() {
   return (
     <div>
+      <div className={classes.Footer}>
+        EarthQuake Live
+      </div>
       <main>
-        This is EarthQuake
+        <div className={classes.main}>
+        Last data
+        EarthQuake Map
+        </div>
       </main>
+      <div className={classes.sidebar}>
+            <div>Latest Earthquake</div>
+            <div>Warning/advisory</div>
+      </div>
+      <Link href="status/">
+          <button className={classes.button}>Go to Status</button>
+        </Link>
       <Footer />
     </div>
   );
