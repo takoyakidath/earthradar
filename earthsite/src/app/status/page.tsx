1116fecf85ca094f82b27eb3b4d45ebc82e8a0d1
TakoyakiDayo <Takoyaki1051@outlook.jp>
2024-07-17 21:28:12 +0900

    good

--- earthsite/src/app/status/page.tsx
+++ earthsite/src/app/status/page.tsx
@@ -5,50 +5,10 @@ import Link from "next/link";
 import classes from "@/components/classes.module.css";
 
 export default function Home() {
-  // true / false
-  function reception() {
-    //30分以内か前回の取得時間を確認しに行く
-    //const ApiTime =
-
-    //とりあえず
-    const ApiTime = true;
-
-    if (ApiTime) {
-      alert("受信しました。!");
-      getData()
-    } else {
-      alert("しばらくしてからもう一度お試しください。");
-    }
-  }
-async function getData(){
-//apiに受信しに行く
-const APIP2P = new WebSocket('wss://api.p2pquake.net/v2/ws');
-
-APIP2P.onopen = function(event) {
-  console.log("OK");
-  const ExportP2P ="OK"
-};
-
-APIP2P.onclose = function(event) {
-  console.log('Code:', event.code);
-};
-
-
-const APIJMA = await fetch("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json")
-if (APIJMA.ok) {
-  console.log("OK")
-  const ExportJma = "OK"
-}else {
-  console.log(APIJMA.status);
-}
-}
   return (
     <div>
       <main>
         <Reception />
-        <button className={classes.button} onClick={reception}>
-          受信
-        </button>
         <br />
         <Link href="/">
           <button className={classes.button}>Go to Page</button>
--- earthsite/src/components/Reception.tsx
+++ earthsite/src/components/Reception.tsx
@@ -1,12 +1,52 @@
 import classes from "@/components/classes.module.css";
+import { useState } from 'react'; // useStateフックをインポートして状態管理を行う
 export function Reception() {
+          // APIのステータスを保持するための状態変数
+          const [exportJma, setExportJma] = useState(null);
+          const [exportP2P, setExportP2P] = useState(null);
+        
+    function reception() {
+        alert("受信しました。!");
+        getData()
+      }
+    async function getData(){
+    //apiに受信しに行く
+    const APIP2P = new WebSocket('wss://api.p2pquake.net/v2/ws');
+    
+    APIP2P.onopen = function(event) {
+      console.log("OK");
+      setExportP2P("OK"); // 接続成功時に状態を更新
+    };
+    
+    APIP2P.onclose = function(event) {
+      console.log('Code:', event.code);
+      setExportP2P(event.code); // 終了時に状態を更新
+    };
+    
+    const APIJMA = await fetch("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json")
+    if (APIJMA.ok) {
+      console.log("OK")
+      setExportJma("OK");
+      
+    }else {
+      console.log(APIJMA.status);
+      setExportJma(APIJMA.status);
+    }
+    }
+
+
+
     return (
       <div>
         <main>
           <code>
-            JMA-API <br />
-            P2P-API
-          </code>
+          <br />
+          JMA-API {exportJma}<br />
+          P2P-API {exportP2P}<br />
+          </code> 
+          <button className={classes.button} onClick={reception}>
+          受信
+        </button>
         </main>
       </div>
     );
