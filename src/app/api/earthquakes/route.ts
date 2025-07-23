7f7d9777620409c9f4aa6c3b5afe152c2f637307
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-07-23 13:17:59 +0900

    fix

--- src/app/api/earthquakes/route.ts
+++ src/app/api/earthquakes/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  const res = await fetch('https://api-v2-sandbox.p2pquake.net/v2/history?codes=551');
+  const res = await fetch("https://api.p2pquake.net/v2/history?codes=551" );
   const data = await res.json();
 
   const filtered = data
